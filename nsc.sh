d6df070eb4b8d488ca75cebd064af04f4ea4e239
Diesanz <diego.sanzr2003@gmail.com>
2024-10-16 19:17:30 +0200

    Cambios función nsc

--- nsc.sh
+++ nsc.sh
@@ -1,14 +1,23 @@
-
 function nsc(){
-    name = $1
-    [-z $name] && {echo "ERROR: faltan campos"; return 1}
-    [[! $name == *.*]] {name=${name}.sh}
+    name=$1  # Captura del primer argumento
 
-    cat <<EOF > $name
+    # Verifica si no se ha proporcionado un nombre
+    if [ -z "$name" ]; then
+        echo "ERROR: faltan campos"
+        return 1
+    fi
+
+    # Verifica si el nombre no contiene un punto
+    if [[ ! $name == *.* ]]; then
+        name="${name}.sh"  # Agrega la extensión ".sh" si no la tiene
+    fi
+
+    # Crea el archivo y escribe el encabezado del script bash
+    cat <<EOF > "$name"
     #!/bin/bash
     EOF
 
-    chmod u+x $name 
-    chmod u+x $name
+    # Da permisos de ejecución al archivo
+    chmod u+x "$name"
 
 }
