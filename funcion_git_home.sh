b8a00cabc3024f627d020d85b15e24ba0c27202a
Diesanz <diego.sanzr2003@gmail.com>
2024-10-21 11:27:04 +0200

    Cambios sobre funcion git

--- funcion_git_home.sh
+++ funcion_git_home.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 function g() {
+    
     comentario=$1
     git add .
     git commit -m "$comentario"
+
+    if [ "$2" == "-p"]; then
+        git push -u origin main
+    fi
+
 }
 
 # Llama a la función con un mensaje
-g "Nuevos scripts"
+g "Cambios sobre funcion git" -p
 
 function home() {
     if [[ "$1" == "--help" ]]; then
